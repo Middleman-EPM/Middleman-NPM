292f947013dc3d14c5aab9cf783d5e7b8d0934ad
Jung Shin <crnboii@gmail.com>
2018-08-15 16:03:37 -0700

    first working prototype

--- routes-mapping.js
+++ routes-mapping.js
@@ -6,24 +6,24 @@ module.exports = mapApp = (req, map) => {
     if (req.app._router.stack[i].route) {
       //add route
       const method = Object.keys(req.app._router.stack[i].route.methods)[0];
-      if (!map[req.app._router.stack[i].route.path]) map[req.app._router.stack[i].route.path] = {};
+      if (!map[req.app._router.stack[i].route.path])
+        map[req.app._router.stack[i].route.path] = {};
       //check to see if route exists
 
       map[req.app._router.stack[i].route.path][method] = [];
-      map[req.app._router.stack[i].route.path][method + '_functions'] = []
+      map[req.app._router.stack[i].route.path][method + '_functions'] = [];
 
       //array of middleware found on route
       req.app._router.stack[i].route.stack.forEach((element, index) => {
-        let name = element.handle.name
-        map[req.app._router.stack[i].route.path][method + '_functions'].push(name.length > 0 ? name : "anonymous")
-
+        let name = element.handle.name;
+        map[req.app._router.stack[i].route.path][method + '_functions'].push(
+          name.length > 0 ? name : 'anonymous'
+        );
       });
-
     }
-
   }
   return map;
-}
+};
 
 /*
 for (let i = 0; i< app._router.stack.length; i++){
