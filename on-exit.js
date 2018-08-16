021c5ca5ee3881b36e340ad83ee5c8db9f2ba635
Jung Shin <crnboii@gmail.com>
2018-08-15 17:38:08 -0700

    bcrypt integration

--- on-exit.js
+++ on-exit.js
@@ -1,10 +1,13 @@
 const axios = require('axios');
+const server = 'http://localhost:8000/';
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 module.exports = onExit = map => {
   for (const prop in map) {
     for (const method in map[prop]) {
       if (!method.includes('_functions'))
         map[prop][method] = [...new Set(map[prop][method])];
-      //console.log(map[prop][method])
     }
   }
   if (process.platform === 'win32') {
@@ -24,16 +27,23 @@ module.exports = onExit = map => {
     let buffer = new Buffer('some content\n');
     const fs = require('fs');
     // Create a writable stream &  Write the data to stream with encoding to be utf8
+    let filename;
+    bcrypt.hash(Date.now(), saltRounds, (err, hash) => {
+      filename = hash;
+    })
+    map.hash = filename;
     const writerStream = fs
-      .createWriteStream(filePath + '/output.json')
-      .on('finish', function() {
+      .createWriteStream(filePath + `/${Date.now()}-middleman.json`)
+      .on('finish', () => {
         console.log('Write Finish.');
-        console.log(filePath);
+        console.log(filename);
+        map.routes = map;
+        axios.post(server, JSON.stringify(map))
+                        .then(response => console.log(response))
+                        .catch(error => console.log(error));
         process.exit();
       })
-      .on('error', function(err) {
-        console.log(err.stack);
-      });
+      .on('error', err => console.log(err.stack));
 
     writerStream.write(JSON.stringify(map), function() {
       // Now the data has been written.
@@ -43,5 +53,4 @@ module.exports = onExit = map => {
 
     writerStream.end();
   });
-  axios.post('http://localhost:8000/', JSON.stringify(map));
 };
